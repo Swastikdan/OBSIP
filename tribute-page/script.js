fa2245168992bfa47ff031285a136c9f81114114
Swastik <57379785+Swastikdan@users.noreply.github.com>
2023-03-05 18:22:29 +0530

    all done

--- tribute-page/script.js
+++ tribute-page/script.js
@@ -3,5 +3,33 @@ const hiddenBtns = document.querySelector('.hidden-btns');
 
 mainBtn.addEventListener('click', () => {
   hiddenBtns.classList.toggle('show');
-  mainBtn.textContent = hiddenBtns.classList.contains('show') ? 'Menu' : 'Menu';
+ 
 });
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+// Select all image elements
+const images = document.querySelectorAll('.relative img');
+
+// Set index to 0
+let index = 0;
+
+// Set interval to scroll images automatically
+setInterval(() => {
+  // Set opacity to 0 on current image
+  images[index].classList.remove('opacity-100');
+  images[index].classList.add('opacity-0');
+  
+  // Increment index
+  index++;
+  
+  // Check if index is greater than or equal to number of images
+  if (index >= images.length) {
+    // Set index to 0
+    index = 0;
+  }
+  
+  // Set opacity to 100 on new image
+  images[index].classList.remove('opacity-0');
+  images[index].classList.add('opacity-100');
+}, 5000);
